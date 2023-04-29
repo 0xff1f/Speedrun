74b7e5cf0e4f8ee0cab222b4a1394cbc3394c495
Dr.X <62835954+0xff1f@users.noreply.github.com>
2023-04-29 20:12:29 +0300

    hhhhhhhhh

---
+++ welcome3.py
@@ -0,0 +1,21 @@
+def welcome():
+    print ('welcome')
+    
+def welcome2():
+    print('this is an addition')
+    
+    
+
+"""
+
+If i want to sync my PC to the Github after i created Github project 
+
+dr.xvirus@DrXViruss-MacBook-Pro Git_Basics % git remote add origin https://github.com/0xff1f/Bitriot.git
+dr.xvirus@DrXViruss-MacBook-Pro Git_Basics % git push -u origin main
+"""
+
+    """
+    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
+    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
+    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    --> This has all ticket seleted to allow all .
+    """
